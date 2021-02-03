10debfd850c1976f5896fc30ea865dce04fe8336
Jamo <jarmokos@gmail.com>
2021-02-03 16:29:50 +0200

    Added covid-19 figures from Forssa

--- backend.js
+++ backend.js
@@ -4,27 +4,26 @@ const { console, Date } = require("globalthis/implementation")
 
 const daylengthreport = document.getElementById("daylength")
 const namereport = document.getElementById("namedate")
-// const weatherreport = document.getElementById("weather")
+const covidreport = document.getElementById("koronadata")
 
 namereport.addEventListener("click", () => {
-    console.log("speaking")
+    console.log(`speaking: ${namereport.innerText}`)
     playtext(namereport.innerText)
 })
 
 daylengthreport.addEventListener("click", () => {
-    console.log("speaking")
+    console.log(`speaking: ${daylengthreport.innerText}`)
     playtext(daylengthreport.innerText)
+})
+
+covidreport.addEventListener("click", () => {
+    console.log(`speaking: ${covidreport.innerText}`)
+    playtext(covidreport.innerText)
 })
 
 const utterance = new SpeechSynthesisUtterance()
 
 utterance.lang = "en"
-// utterance.addEventListener('end', () => {
-//     txtIn.disabled = false
-// })
-// utterance.addEventListener("boundary", e => {
-//     currentChar = e.charIndex
-// })
 
 function playtext(text) {
     if (speechSynthesis.speaking) {
@@ -49,15 +48,22 @@ async function getWebSiteData(site) {
     }
 }
 
+async function getWebJson(site) {
+    const response = await fetch(site)
+    if (response.ok) {
+        const receivedData = await response.json()
+        return receivedData
+    } else {
+        return "No data received"
+    }
+}
+
 async function getDayLength() {
     const getWebData = await getWebSiteData("https://www.viastar.fi/Aurinko_nousee_laskee/Forssa/") // Option 1, correct timezone
-    // const getWebData = await getWebSiteData("https://www.auringonlasku.fi/forssa") // Option 2, timezone 1h off (amsterdam)
     try {
         const $ = cheerio.load(getWebData)
         const sunRise = $("body > table:nth-child(1) > tbody > tr > td:nth-child(1) > table > tbody > tr > td > b > font").text().match(/\d{1,2}:\d{1,2}/)
         const sunDown = $("body > table:nth-child(1) > tbody > tr > td:nth-child(1) > table > tbody > tr > td > p > font:nth-child(1) > b").text().match(/\d{1,2}:\d{1,2}/)
-        // const sunRise = $("body > div:nth-child(2) > div.row > div:nth-child(1) > p:nth-child(2)").text().match(/\d{1,2}:\d{1,2}/) // Option 2
-        // const sunDown = $("body > div:nth-child(2) > div.row > div:nth-child(1) > p:nth-child(4)").text().match(/\d{1,2}:\d{1,2}/) // Option 2
         daylengthreport.innerHTML = `Aurinko nousee ${sunRise} ja laskee ${sunDown}`
     } catch (e) {
         daylengthreport.innerText = "Ei saa parsittua päivän pituutta"
@@ -78,13 +84,17 @@ async function nameDateToday() {
     }
 }
 
-// async function getWeather() {
-//     const getWebData = await getWebSiteData(`https://www.foreca.fi/Finland/Forssa/10vrk`)
-//     const $ = cheerio.load(getWebData)
-//     const weatherdata = $("body").find("#tenday").text()
-//     console.log(weatherdata)
-//     weatherreport.innerHTML = `${weatherdata}`
-// }
+async function currentCovidStatus() {
+    const getCovidData = await getWebJson(`https://sampo.thl.fi/pivot/prod/fi/epirapo/covid19case/fact_epirapo_covid19case.json?row=hcdmunicipality2020-444995`)
+    try {
+        const totalCases = getCovidData["dataset"]["value"][0]
+        covidreport.innerText = `Forssassa on ${totalCases} todettua koronatapausta`
+    } catch (e) {
+        covidreport.innerText = "Ei saatu COVID-19 tapauksista haettua tietoa"
+        console.log(e)
+    }
+}
 
 getDayLength()
 nameDateToday()
+currentCovidStatus()
